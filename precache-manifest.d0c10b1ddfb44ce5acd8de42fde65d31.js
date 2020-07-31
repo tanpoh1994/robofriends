a2eff4b4e24920ec2ab80fe3334162cd59a68fa9
tanpoh1994 <tpctpc1994@gmail.com>
2020-07-31 21:15:03 +0800

    Updates

---
+++ precache-manifest.d0c10b1ddfb44ce5acd8de42fde65d31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3ecd35377ce5fbe956d32d40ba2631",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "aebb2a1923f94ba21b0a",
+    "url": "/robofriends/static/css/main.eccf75d5.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aebb2a1923f94ba21b0a",
+    "url": "/robofriends/static/js/main.0de87d90.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
